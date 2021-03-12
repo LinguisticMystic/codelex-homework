e195090167d180599739668b09bb0eb8536ce3d3
Aija Pastare <aija.pastare@gmail.com>
2021-03-12 17:15:23 +0200

    Car race interface update

--- PHP/tasks/car-race/Movable.php
+++ PHP/tasks/car-race/Movable.php
@@ -2,7 +2,11 @@
 
 Interface Movable
 {
+    public function getName(): string;
     public function increaseDistance(): void;
+    public function getDistanceTraveled(): int;
     public function crash(): void;
     public function stop(): void;
+    public function hasStopped(): bool;
+    public function hasCrashed(): bool;
 }
