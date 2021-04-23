b9671755f2db085ed6753cb8b7868d0d7afa61e0
LinguisticMystic <aija.pastare@gmail.com>
2021-04-23 19:03:03 +0300

    Middlewares added

--- PHP/tasks/tinder/bootstrap/middlewares.php
+++ PHP/tasks/tinder/bootstrap/middlewares.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Controllers\DashboardController;
+use App\Controllers\DislikesController;
 use App\Controllers\EditGalleryController;
+use App\Controllers\FavoritesController;
 use App\Controllers\HomeController;
 use App\Controllers\ProfileController;
+use App\Controllers\RegisterController;
+use App\Controllers\ViewUserProfileController;
 use App\Middlewares\AuthenticationMiddleware;
 use App\Middlewares\CheckLogoutMiddleware;
 
@@ -17,7 +21,19 @@ return [
     EditGalleryController::class . '@index' => [
         AuthenticationMiddleware::class
     ],
+    FavoritesController::class . '@index' => [
+        AuthenticationMiddleware::class
+    ],
+    DislikesController::class . '@index' => [
+        AuthenticationMiddleware::class
+    ],
+    ViewUserProfileController::class . '@index' => [
+        AuthenticationMiddleware::class
+    ],
     HomeController::class . '@index' => [
+        CheckLogoutMiddleware::class
+    ],
+    RegisterController::class . '@index' => [
         CheckLogoutMiddleware::class
     ],
 ];
